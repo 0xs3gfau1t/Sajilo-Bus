eb4d0ef04c7e11918e2d210dae9fe00710e2b249
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-03 00:50:00 +0545

    transaction handlers

---
+++ client/src/redux/actions/tx.js
@@ -0,0 +1,59 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
+
+export const searchTx = createAsyncThunk(
+	"tx/search",
+	async ({ id, page, reload = false }, { getState, dispatch }) => {
+		const { tx } = getState()
+		const take = 2
+
+		// If data is already present don't fetch the list
+		const previousData = tx[tx.mode][id][page]
+		if (previousData && !reload) {
+			const amount = Object.keys(previousData).length
+			return {
+				success: true,
+				id,
+				page,
+				canPrev: page != 0,
+				canNext: amount == take,
+			}
+		}
+
+		const url = `/api/${tx.mode}/tx`
+		const params = {
+			[tx.mode == "bus" ? "bus_number" : "id"]: id,
+			skip: page * take,
+			take,
+		}
+
+		const { transaction } = await axios
+			.get(url, { params, withCredentials: true })
+			.then(res => res.data)
+			.catch(err => {
+				console.error(err)
+			})
+
+		if (!transaction) return { success: false }
+
+		const amount = Object.keys(transaction).length
+
+		if (amount == 0)
+			return {
+				success: true,
+				canNext: false,
+				canPrev: true,
+				page: tx.currentPage,
+				id,
+			}
+
+		return {
+			success: true,
+			transaction,
+			page,
+			id,
+			canNext: amount == take,
+			canPrev: page != 0,
+		}
+	}
+)

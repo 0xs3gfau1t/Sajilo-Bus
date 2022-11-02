57028f211f5798336558751dbff3dd058676abdf
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-02 22:05:19 +0545

    khalti api integration

--- server/routes/card/balance.js
+++ server/routes/card/balance.js
@@ -1,4 +1,5 @@
 const db = require("../../prisma")
+const verifyPayment = require("../../utils/verifyPayment")
 
 const balanceEnquiry = async (req, res) => {
 	const { id } = req.query
@@ -21,13 +22,26 @@ const balanceEnquiry = async (req, res) => {
 const balanceLoad = async (req, res) => {
 	var { id, amount, token } = req.body
 
+	if (!id || !amount || !token)
+		return res.status(400).json({ message: "Missing params." })
+
 	amount = Number(amount)
+
+	const valid = await verifyPayment(token, amount)
+
+	if (!valid)
+		return res
+			.status(400)
+			.json({ message: "Couldnot verify the transaction." })
 
 	try {
 		const card = await db.card.update({
 			where: { id },
 			data: { balance: { increment: amount } },
 		})
+
+		if (!card) return res.status(400).json({ message: "Invalid card." })
+
 		return res
 			.status(200)
 			.json({ message: "Money loaded.", balance: card.balance })
--- server/routes/card/buy.js
+++ server/routes/card/buy.js
@@ -1,9 +1,20 @@
 const db = require("../../prisma")
 const buildCard = require("../../utils/buildCard")
+const verifyPayment = require("../../utils/verifyPayment")
 
 const buyHandler = async (req, res) => {
-	var { amount } = req.body // In paisa
+	var { amount, token } = req.body // In paisa
+	if (!amount || !token)
+		return res.status(400).json({ message: "Missing params" })
+
 	amount = Number(amount)
+
+	const valid = verifyPayment(amount, token)
+
+	if (!valid)
+		return res
+			.status(400)
+			.json({ message: "Couldnot verify the transaction." })
 
 	if (amount < 10000)
 		return res.status(400).json({ message: "Insufficient amount." })
@@ -12,6 +23,7 @@ const buyHandler = async (req, res) => {
 		const card = await db.card.create({ data: { balance: amount } })
 
 		if (!card) return res.status(400).json({ message: "Invalid card id." })
+
 		return res.status(200).send(buildCard(card.id))
 	} catch (err) {
 		console.log(err)

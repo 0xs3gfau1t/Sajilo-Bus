71e9bf47bdd4c60178c33bf5ba6a8782d4c16d1e
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-02 17:27:36 +0545

    add buss

--- server/routes/bus/index.js
+++ server/routes/bus/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const loginHandler = require("./login")
 const infoHandler = require("./info")
+const addHandler = require("./add")
 
 router.post("/login", loginHandler)
 router.get("/info", infoHandler)
+router.post("/add", addHandler)
 
 module.exports = router

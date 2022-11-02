9effe4910da8b5e706b9e082ec654ef545ba878a
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-02 17:47:08 +0545

    remove bus

--- server/routes/bus/index.js
+++ server/routes/bus/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const loginHandler = require("./login")
 const infoHandler = require("./info")
 const addHandler = require("./add")
+const removeBusHandler = require("./remove")
 
 router.post("/login", loginHandler)
 router.get("/info", infoHandler)
 router.post("/add", addHandler)
+router.delete("/remove", removeBusHandler)
 
 module.exports = router

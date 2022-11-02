3f1853ab734f587ec4bf31c80a720f00c1857614
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-02 18:18:26 +0545

    list buses route

--- server/routes/bus/index.js
+++ server/routes/bus/index.js
@@ -6,11 +6,13 @@ const infoHandler = require("./info")
 const addHandler = require("./add")
 const removeBusHandler = require("./remove")
 const TxHandler = require("./Tx")
+const listHandler = require("./list")
 
 router.post("/login", loginHandler)
 router.get("/info", infoHandler)
 router.post("/add", addHandler)
 router.delete("/remove", removeBusHandler)
 router.get("/tx", TxHandler)
+router.get("/list", listHandler)
 
 module.exports = router

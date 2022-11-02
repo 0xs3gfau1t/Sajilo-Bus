382130833ca908ef952b1cc6535e0a402ca0b2f2
bimarshak7 <khanalbimarsha7@gmail.com>
2022-11-02 14:46:47 +0545

    Workspace setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App">
+			<h1>U-TECH Hackathon</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+	console.log(env.BASE_URL)
+	return defineConfig({
+		plugins: [react()],
+		envPrefix: "SAJILO",
+		server: {
+			open: true,
+			port: env.PORT,
+			proxy: {
+				"/api": {
+					target: env.BASE_URL,
+					changeOrigin: true,
+				},
+			},
+		},
+	})
+}

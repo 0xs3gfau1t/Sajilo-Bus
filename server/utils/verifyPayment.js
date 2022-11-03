20036b698d1b5c5495ef9693a3583fdfb5e4b6e6
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-03 19:13:23 +0545

    haha try nagare ni hunxa revoke vaisakyo

--- server/utils/verifyPayment.js
+++ server/utils/verifyPayment.js
@@ -1,24 +1,24 @@
 const axios = require("axios")
 
 const verifyPayment = async (amount, token) => {
+	console.log({ amount, token })
 	try {
 		var config = {
 			method: "post",
 			url: "https://khalti.com/api/v2/payment/verify/",
 			headers: {
-				"Authorization":
-					"Key test_secret_key_287d4dad492c439ebc9ddb1d8f3ab15d",
+				"Authorization": `Key ${process.env.KHALTI_SECRET}`,
 				"Content-Type": "application/json",
 			},
-			data: { amount, token },
+			data: { amount: Number(amount), token },
 		}
 		const response = await axios(config)
 			.then(res => {
-				// console.log("Success", res.data)
+				console.log("Success", res.data)
 				return true
 			})
 			.catch(err => {
-				// console.log("Error", err)
+				console.log("Error", err.response.data)
 				return false
 			})
 
